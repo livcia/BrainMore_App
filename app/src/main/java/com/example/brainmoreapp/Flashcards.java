890e3ddecce0455f415f656b022a1da2ce4c6379
Oliwka <ankiewicz_oliwia@wp.pl>
2023-06-03 10:42:30 +0200

    Added New Activities and (public voids)Intents. Added the same background to all activities.

---
+++ app/src/main/java/com/example/brainmoreapp/Flashcards.java
@@ -0,0 +1,14 @@
+package com.example.brainmoreapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Flashcards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flashcards);
+    }
+}
