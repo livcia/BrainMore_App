d07ffdd7940c30f58f89bcd8409fbc38962d4e88
Oliwka <ankiewicz_oliwia@wp.pl>
2023-06-04 14:48:23 +0200

    Added usable arrows (back to main menu) to all activities.

--- app/src/main/java/com/example/brainmoreapp/Quiz.java
+++ app/src/main/java/com/example/brainmoreapp/Quiz.java
@@ -2,7 +2,9 @@ package com.example.brainmoreapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Quiz extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+    }
+    public void clickEvent(View v)
+    {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
