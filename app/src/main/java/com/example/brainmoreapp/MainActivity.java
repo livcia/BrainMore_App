dde5f925166a3f6d894324717f96f64bf3f70efa
Oliwka <ankiewicz_oliwia@wp.pl>
2023-06-07 07:38:19 +0200

    more readable code

--- app/src/main/java/com/example/brainmoreapp/MainActivity.java
+++ app/src/main/java/com/example/brainmoreapp/MainActivity.java
@@ -20,24 +20,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);/*
+                 _                                  _            __
+                (_)                                | |          / _|
+   __ _ ___ ___ _  __ _ _ __  _ __ ___   ___ _ __ | |_    ___ | |_  __   ____ _ _ __
+  / _` / __/ __| |/ _` | '_ \| '_ ` _ \ / _ \ '_ \| __|  / _ \|  _| \ \ / / _` | '__|
+ | (_| \__ \__ \ | (_| | | | | | | | | |  __/ | | | |_  | (_) | |    \ V / (_| | |
+  \__,_|___/___/_|\__, |_| |_|_| |_| |_|\___|_| |_|\__|  \___/|_|     \_/ \__,_|_|
+                   __/ |
+                   |___/*/
+
         language = findViewById(R.id.Lng);
         OpenFlashCards = findViewById(R.id.flashcards);
         OpenQuiz = findViewById(R.id.quiz);
-        OpenWordGame = findViewById(R.id.wordgame);
+        OpenWordGame = findViewById(R.id.wordgame);/*
+  ______ _                           _ ______               ___
+ / _____) |                         | (_____ \             / __)
+( (____ | |__  _____  ____ _____  __| |_____) )___ _____ _| |__ _____  ____ _____ ____   ____ _____  ___
+ \____ \|  _ \(____ |/ ___) ___ |/ _  |  ____/ ___) ___ (_   __) ___ |/ ___) ___ |  _ \ / ___) ___ |/___)
+ _____) ) | | / ___ | |   | ____( (_| | |   | |   | ____| | |  | ____| |   | ____| | | ( (___| ____|___ |
+(______/|_| |_\_____|_|   |_____)\____|_|   |_|   |_____) |_|  |_____)_|   |_____)_| |_|\____)_____|___/
+
+*/
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
         editor = pref.edit();
         editor.apply();
         Boolean Ln = pref.getBoolean("BLang", false);
         language.setChecked(Ln);
         getLanguage(Ln);
-
         language.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 getLanguage(language.isChecked());
                 editor.putBoolean("BLang", language.isChecked()).commit();
             }
         });
+
         OpenFlashCards.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Flashcards.class);
