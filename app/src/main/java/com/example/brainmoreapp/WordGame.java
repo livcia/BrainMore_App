6bd16ed2c1824721824ae57b20056c8b19de4c39
Oliwka <ankiewicz_oliwia@wp.pl>
2023-06-09 08:48:42 +0200

    add Arrays with words in WordGame.java

--- app/src/main/java/com/example/brainmoreapp/WordGame.java
+++ app/src/main/java/com/example/brainmoreapp/WordGame.java
@@ -5,17 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class WordGame extends AppCompatActivity {
-
+    String[] PLWords = new String[] {"PLANETA", "DELFIN", "FIGURA"};
+    String[] ENGWords = new String[] {"FAIRLY", "TRUSTY", "MISFIT"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_game);
         Intent intent = getIntent();
-        Boolean lang = intent.getBooleanExtra("PL_or_ENG",false);
+        Boolean lang = intent.getBooleanExtra("PL_or_ENG",false);;
     }
     public void clickEvent(View v)
     {
