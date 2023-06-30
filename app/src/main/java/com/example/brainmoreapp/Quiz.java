250853f4c01f6ae5c0be79599fcd6b5b75d5dc89
Oliwka <ankiewicz_oliwia@wp.pl>
2023-07-01 00:16:57 +0200

    add List<String> Listquestions in Quiz.java

--- app/src/main/java/com/example/brainmoreapp/Quiz.java
+++ app/src/main/java/com/example/brainmoreapp/Quiz.java
@@ -5,15 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Quiz extends AppCompatActivity {
-
+    TextView TextViewquestion;
+    Button b1, b2, b3, b4;
+    Random rand;
+    int indexofquestion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String[] questions = new String[] {
+                "abcd",
+                "efgh",
+                "ijkl",
+                "mnop",
+                "rstu",
+                "wxyz"
+        };
+        List<String> Listquestions = new ArrayList<>(Arrays.asList(questions));
+        b1 = findViewById(R.id.b1);
+        b2 = findViewById(R.id.b2);
+        b3 = findViewById(R.id.b3);
+        b4 = findViewById(R.id.b4);
+        rand = new Random();
+        indexofquestion = rand.nextInt(Listquestions.size());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         Intent intent = getIntent();
         Boolean lang = intent.getBooleanExtra("PL_or_ENG",false);
+        TextViewquestion = findViewById(R.id.ask);
     }
     public void clickEvent(View v)
     {
