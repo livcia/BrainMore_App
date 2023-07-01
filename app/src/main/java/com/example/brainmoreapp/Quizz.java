6c7d5efc33b76540082544b154b23cd10630e4b2
Oliwka <ankiewicz_oliwia@wp.pl>
2023-07-02 00:58:12 +0200

    rewrite code to new files

--- app/src/main/java/com/example/brainmoreapp/Quizz.java
+++ app/src/main/java/com/example/brainmoreapp/Quizz.java
@@ -2,16 +2,23 @@ package com.example.brainmoreapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 public class Quizz extends AppCompatActivity {
     String[] questions, answers;
     Map<String, String> map;
     Button btn1, btn2, btn3, btn4;
+    Random rand;
+    int indexofquestion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +39,10 @@ public class Quizz extends AppCompatActivity {
                 "r", "s", "t", "u",
                 "w", "x", "y", "z"
         };
+        Intent intent = getIntent();
+        Boolean lang = intent.getBooleanExtra("PL_or_ENG",false);
         map = new HashMap<String, String>();
+        List<String> Listquestions = new ArrayList<>(Arrays.asList(questions));
         map.put("abcd", "d");
         map.put("efgh", "h");
         map.put("ijkl", "l");
@@ -44,5 +54,7 @@ public class Quizz extends AppCompatActivity {
         btn3 = findViewById(R.id.b3);
         btn4 = findViewById(R.id.b4);
         btn1.setText("dd");
+        rand = new Random();
+        indexofquestion = rand.nextInt(Listquestions.size());
     }
 }
